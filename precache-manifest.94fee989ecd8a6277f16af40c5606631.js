808a88f72888319783a9555069a0ebd38dc330be
Forrest Heath 3 <forrest@pareto-design.com>
2019-06-29 22:19:45 -0500

    Updates

---
+++ precache-manifest.94fee989ecd8a6277f16af40c5606631.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "950ab288a9818e29f00968c54a3cfed7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "2da6b26815100f1573e7",
+    "url": "/static/css/2.619dee99.chunk.css"
+  },
+  {
+    "revision": "80be3137b7a180ba3462",
+    "url": "/static/css/main.b313eecd.chunk.css"
+  },
+  {
+    "revision": "2da6b26815100f1573e7",
+    "url": "/static/js/2.6a30a55c.chunk.js"
+  },
+  {
+    "revision": "80be3137b7a180ba3462",
+    "url": "/static/js/main.87f36d55.chunk.js"
+  },
+  {
+    "revision": "42ac5946195a7306e2a5",
+    "url": "/static/js/runtime~main.a8a9905a.js"
+  },
+  {
+    "revision": "5b15a8c6575231a6683cc8982faab6eb",
+    "url": "/static/media/dai.5b15a8c6.jpg"
+  }
+]);
