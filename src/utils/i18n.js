2dba64e88100484839ac2196b946c46e8b5981d2
Forrest Heath 3 <forrest@pareto-design.com>
2019-07-06 19:28:25 -0500

    reverted the laungage switcher back to the original logic

--- src/utils/i18n.js
+++ src/utils/i18n.js
@@ -3,20 +3,25 @@ import LanguageDetector from "i18next-browser-languagedetector";
 import { reactI18nextModule } from "react-i18next";
 
 i18n
-  // load translation using xhr -> see /public/locales
-  // learn more: https://github.com/i18next/i18next-xhr-backend
-
   .use(LanguageDetector)
-  // pass the i18n instance to react-i18next.
   .use(reactI18nextModule)
-  // init i18next
-  // for all options read: https://www.i18next.com/overview/configuration-options
   .init({
-    Lng: 'es',
-    debug: true,
+    fallbackLng: "es",
+
+    resources: {
+      en: { translations: require("../locales/es/translations.json") },
+      es: { translations: require("../locales/es/translations.json") }
+    },
+
+    ns: ["translations"],
+    defaultNS: "translations",
 
     interpolation: {
-      escapeValue: false, // not needed for react as it escapes by default
+      escapeValue: false
+    },
+
+    react: {
+      wait: true
     }
   });
 
